e0a99525013ea2db4a81490ab44e4df16efa7fbf
Ehab Eldeeb <124740573+EhabEldeeb@users.noreply.github.com>
2024-11-09 16:25:12 +0200

    Update fmi.php

--- fmi.php
+++ fmi.php
@@ -1,5 +1,14 @@
 <?php
-$myUrl = "https://freemyip.com/update?verbose=yes&" . $_SERVER['QUERY_STRING'];
+$requestIP = $_SERVER['REMOTE_ADDR'];
+$myUrl = "https://freemyip.com/update?";
+if (isset($_GET['myip']) && $_GET['myip'] == "")
+  $_GET['myip'] = $_SERVER['REMOTE_ADDR'];
+$_GET['verbose'] = 'yes';
+$QueryString = [];
+foreach ($_GET as $k => $v) {
+  $QueryString[] = $k . "=" . urlencode($v);
+}
+$myUrl .= implode("&", $QueryString);
 $ch = curl_init($myUrl);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
